8d5636939f78ebb6d495db6e757cac93bc22cd0c
Lawrence Foley <emwav333@gmail.com>
2015-03-21 21:54:25 -0500

    Added cameraScript

---
+++ project-2/Assets/scripts/cameraScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraScript : MonoBehaviour {
+
+	public Transform player;
+	public float distanceFromPlayer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 moveToPosition = Vector3.zero;
+
+		moveToPosition.z = player.position.z - distanceFromPlayer;
+		moveToPosition.x = player.position.x;
+		moveToPosition.y = player.position.y + distanceFromPlayer;
+
+		transform.position = moveToPosition;
+	}
+}
