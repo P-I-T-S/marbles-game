6e04a8ad2b566038f9aa150ffb4d3385e28ac609
Lawrence Foley <emwav333@gmail.com>
2015-03-21 21:27:10 -0500

    Added ballMovement script

---
+++ project-2/Assets/scripts/ballMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballMovement : MonoBehaviour {
+	public float speed = 5f;
+	Rigidbody rigidbody;
+	float xInput;
+	float zInput;
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		xInput = Input.GetAxis ("Horizontal");
+		zInput = Input.GetAxis ("Vertical");
+		Vector3 moveDir = new Vector3 (xInput, 0, zInput);
+		
+		//transform.Translate (moveDir * speed * Time.deltaTime);
+
+		rigidbody.AddForce (moveDir * speed);
+
+	}
+}
