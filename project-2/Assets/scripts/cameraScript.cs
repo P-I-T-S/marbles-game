38900c490a1dad7ac9fee9df53ce2f08045c590d
Lawrence Foley <emwav333@gmail.com>
2015-03-21 22:13:31 -0500

    Added rotation to camera script

--- project-2/Assets/scripts/cameraScript.cs
+++ project-2/Assets/scripts/cameraScript.cs
@@ -5,20 +5,30 @@ public class cameraScript : MonoBehaviour {
 
 	public Transform player;
 	public float distanceFromPlayer;
+	public float turnSpeed = 4f;
+	private Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
-	
+		offset = new Vector3 (player.position.x, player.position.y + 8.0f, player.position.z + 8.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Vector3 moveToPosition = Vector3.zero;
 
-		moveToPosition.z = player.position.z - distanceFromPlayer;
-		moveToPosition.x = player.position.x;
-		moveToPosition.y = player.position.y + distanceFromPlayer;
+//		moveToPosition.z = player.position.z - distanceFromPlayer;
+//		moveToPosition.x = player.position.x;
+//		moveToPosition.y = player.position.y + distanceFromPlayer;
+//
+//		transform.position = moveToPosition;
 
-		transform.position = moveToPosition;
+
+	}
+	void LateUpdate()
+	{
+		offset = Quaternion.AngleAxis (Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+		transform.position = player.position + offset;
+		transform.LookAt(player.position);
 	}
 }
