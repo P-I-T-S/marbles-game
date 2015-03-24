a4f8b13ed1166c54a0dca4c89157cb2726ea64e0
Coleten McGuire <s505828@mail.nwmissouri.edu>
2015-03-23 23:13:37 -0500

    ball jumps when space is pressed
    You can change the amount of force the ball jumps with as well.

---
+++ project-2/Assets/scripts/ballJump.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballJump : MonoBehaviour {
+	public float jumpForce;
+	Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Vector3 jump = new Vector3(0, jumpForce, 0);
+			rigidbody.AddForce(jump);
+		}
+	}
+}
