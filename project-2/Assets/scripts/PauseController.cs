ab38600b8586fb6d3bb9f3fd37ca4e9c04fee812
Keswiik <littleman74@live.com>
2015-03-27 15:12:13 -0500

    Pause menu & ball friction
    There's an odd bug with the pause menu - sometimes the ball will
    unexpectedly increase in speed after un-pausing. Haven't figured out
    what's causing it yet.

---
+++ project-2/Assets/scripts/PauseController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseController : MonoBehaviour {
+    bool isPaused, buttonDown;
+    public Canvas pauseMenu;
+
+	// Use this for initialization
+	void Start () {
+        isPaused = false;
+        pauseMenu.enabled = isPaused;
+        buttonDown = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Escape) && !buttonDown)
+        {
+            isPaused = !isPaused;
+            pauseMenu.enabled = isPaused;
+            buttonDown = true;
+            Time.timeScale = isPaused ? 0 : 1;
+        }
+        else if (buttonDown)
+        {
+            buttonDown = false;
+        }
+	}
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+        Application.LoadLevel(Application.loadedLevelName);
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+        //Application.LoadLevel("MainMenuSceneName");
+    }
+}
