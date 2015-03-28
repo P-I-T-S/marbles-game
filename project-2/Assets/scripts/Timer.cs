5de2f473c5c7af9c2e85432cd80bf674a9b50ff8
Coleten McGuire <s505828@mail.nwmissouri.edu>
2015-03-28 14:18:25 -0500

    basic in game timer

---
+++ project-2/Assets/scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+    public static float timer;
+	// Use this for initialization
+	void Start () {
+        
+	}
+    void OnGUI()
+    {
+        int minutes = Mathf.FloorToInt(timer / 60F);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+
+        string Time = string.Format("{0:0}:{1:00}", minutes, seconds);
+        GUI.skin.label.fontSize = 50;
+        GUI.color = Color.black;
+        GUI.Label(new Rect(475, 0, 250, 250), Time);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+	}
+}
