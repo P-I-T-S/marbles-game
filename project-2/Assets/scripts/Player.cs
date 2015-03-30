863b2725a0c2b0a8b6199cf6ea9d76462d66984d
Lawrence Foley <emwav333@gmail.com>
2015-03-29 23:31:04 -0500

    Loads highscores data

---
+++ project-2/Assets/scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player {
+
+    public string Name { get; set; }
+    public float Time { get; set; }
+
+    public Player()
+    {
+        Name = "";
+        Time = 0.0f;
+    }
+
+    public Player(string name, float time)
+    {
+        this.Name = name;
+        this.Time = time;
+    }
+
+
+}
---
+++ project-2/Assets/scripts/gameManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class gameManager : MonoBehaviour {
+
+    // The names of the levels to load from the saved highscore data
+    string[] registeredLevelNames = { "Level_1", "Level_2" };
+    // A dictionary with the level name as the key and the list of players as the values
+    Dictionary<string, Player[]> highscores = new Dictionary<string, Player[]>();
+
+    void Awake()
+    {
+        // Makes sure the Game Manager persists while the game is running
+        GameObject gameManager = GameObject.FindGameObjectWithTag("Game Manager");
+        DontDestroyOnLoad(gameManager);
+
+        if(PlayerPrefs.GetString(registeredLevelNames[0]) == "")
+        {
+            Debug.Log("err");
+        }
+
+        // Load the highscores
+        foreach(string levelName in registeredLevelNames)
+        {
+            // If this is the first time the game has been played, load filler data
+            if (PlayerPrefs.GetString(levelName) == "")
+            {
+                PlayerPrefs.SetString(levelName, "Lawrence:0.0 Lawrence:0.0 Lawrence:0.0 Lawrence:0.0 Lawrence:0.0");
+                PlayerPrefs.Save();
+            }
+            else
+            {
+                string highscoreData = PlayerPrefs.GetString(levelName);
+                highscores.Add(levelName, parsePlayerData(highscoreData));
+            }
+        }
+    }
+
+    private Player[] parsePlayerData(string data)
+    {
+        // There are always 5 highscores per level
+        Player [] players = new Player [5];
+        // Split the string into players
+        string[] playersString = data.Split(' ');
+        
+        // Test if the string array is the right length
+        if(playersString.Length == 5)
+        {
+            int count = 0;
+            // Create players for each player in string
+            foreach(string player in playersString)
+            {
+                string[] playerValues = player.Split(':');
+                Player p = new Player(playerValues[0], float.Parse(playerValues[1]));
+                players[count] = p;
+                count ++;
+            }
+        }
+        else
+        {
+            Debug.Log("There was an error parsing player data: There was not exactly 5 highscores");
+        }
+
+        return players;
+    }
+
+}
