dda9e250eff07e95538f3e3c057fc8930e65a906
Coleten McGuire <s505828@mail.nwmissouri.edu>
2015-04-03 10:38:18 -0500

    my workspace

---
+++ project-2/Assets/scripts/collisions.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collisions : MonoBehaviour {
+    GameObject superJump;
+    float superJumpForce;
+    int numSuperJump;
+    void Start()
+    {
+        superJump = GameObject.FindGameObjectWithTag("Spring");
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Spring")
+        {
+            superJumpForce = 800f;
+            numSuperJump = 1;
+            if (numSuperJump == 1)
+            {
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    Vector3 jump = new Vector3(0, superJumpForce, 0);
+                    GetComponent<Rigidbody>().AddForce(jump);
+                    numSuperJump = 0;
+                }
+            }
+            col.gameObject.SetActive(false);
+            Invoke("Display", 5);
+        }
+    }
+
+    void Display()
+    {
+        superJump.SetActive(true);
+    }
+}
