23707c9030bf33ef6d538af63d566bf92c0cd515
Lawrence Foley <emwav333@gmail.com>
2015-04-04 19:00:03 -0500

    GUI shows number of gems to collect

--- project-2/Assets/scripts/collisions.cs
+++ project-2/Assets/scripts/collisions.cs
@@ -1,30 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class collisions : MonoBehaviour {
     GameObject superJump;
     float superJumpForce = 800f;
     bool canSuperJump;
-    void Start()
+    Text gemsCollectedText;
+    int numberOfGemsCollected;
+    int totalNumberOfGems;
+
+    // Initialization
+    void Awake()
     {
         superJump = GameObject.FindGameObjectWithTag("Spring");
+        GameObject gemsCollectedUI = GameObject.FindGameObjectWithTag("gemsCollectedGuiText");
+        gemsCollectedText = gemsCollectedUI.GetComponent<Text>();
+        numberOfGemsCollected = 0;
+        totalNumberOfGems = GameObject.FindGameObjectsWithTag("Gem").Length;
     }
 
+    void Start()
+    {
+
+    }
+
+    // Collision events
     void OnTriggerEnter(Collider col)
     {
+        // Super Jump pickup
         if (col.gameObject.tag == "Spring")
         {
             canSuperJump = true;
             col.gameObject.SetActive(false);
             Invoke("Display", 20);
         }
+        // Gem pickup
         else if(col.gameObject.tag == "Gem")
         {
             GameObject.Destroy(col.gameObject);
             Debug.Log("Gem collected!");
-            // Need to update GUI to show gem has been collected
+            numberOfGemsCollected++;
         }
     }
+
+    // Update GUI elements
+    void OnGUI()
+    {
+        // Update number of gems collected
+        gemsCollectedText.text = numberOfGemsCollected.ToString() + "/" + totalNumberOfGems.ToString();
+    }
+
     void Update()
     {
         if (canSuperJump)
