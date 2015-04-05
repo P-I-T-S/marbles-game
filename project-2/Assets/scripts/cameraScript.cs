eefe95e97b84b40feca204c03938ed9f1559187e
Coleten McGuire <s505828@mail.nwmissouri.edu>
2015-04-05 03:13:10 -0500

    feel free to work with camerascript

--- project-2/Assets/scripts/cameraScript.cs
+++ project-2/Assets/scripts/cameraScript.cs
@@ -12,6 +12,7 @@ public class cameraScript : MonoBehaviour {
 	public float turnSpeed = 4f;
 	private Vector3 offset;
     float rotationY = 0f;
+    float rotationX = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -32,14 +33,20 @@ public class cameraScript : MonoBehaviour {
 	{
         player = GameObject.FindGameObjectWithTag("Player").transform;
 
-		offset = Quaternion.AngleAxis (Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
-		transform.position = player.position + offset;
-		transform.LookAt(player.position);
-
-        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, -Vector3.right) * offset;
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
         transform.position = player.position + offset;
         transform.LookAt(player.position);
-        
+
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, -Vector3.right) * offset;
+        Vector3 pos = offset;
+        pos.y = Mathf.Clamp(pos.y, -15f, 15f);
+        offset = pos;
+        transform.position = player.position + offset;
+        Vector3 playerPOS = player.position;
+        playerPOS.y = Mathf.Clamp(playerPOS.y, -15f, 15f);
+        player.position = playerPOS;
+        transform.LookAt(new Vector3(player.position.x, Mathf.Clamp(player.position.y, -15f, 15f), player.position.z));
+
 
         //rotationY += Input.GetAxis("Mouse Y") * turnSpeed;
         //rotationY = Mathf.Clamp(rotationY, -45f, 45f);
