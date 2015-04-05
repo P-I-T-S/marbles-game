b9a3b5e7849158b8e1c4716d599a6b77bb5f91b5
Coleten McGuire <s505828@mail.nwmissouri.edu>
2015-04-05 04:12:43 -0500

    my workspace

--- project-2/Assets/scripts/cameraScript.cs
+++ project-2/Assets/scripts/cameraScript.cs
@@ -38,14 +38,18 @@ public class cameraScript : MonoBehaviour {
         transform.LookAt(player.position);
 
         offset = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, -Vector3.right) * offset;
-        Vector3 pos = offset;
-        pos.y = Mathf.Clamp(pos.y, -15f, 15f);
-        offset = pos;
         transform.position = player.position + offset;
-        Vector3 playerPOS = player.position;
-        playerPOS.y = Mathf.Clamp(playerPOS.y, -15f, 15f);
-        player.position = playerPOS;
-        transform.LookAt(new Vector3(player.position.x, Mathf.Clamp(player.position.y, -15f, 15f), player.position.z));
+        transform.LookAt(player.position);
+
+        //offset = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, -Vector3.right) * offset;
+        //Vector3 pos = offset;
+        //pos.y = Mathf.Clamp(pos.y, -15f, 15f);
+        //offset = pos;
+        //transform.position = player.position + offset;
+        //Vector3 playerPOS = player.position;
+        //playerPOS.y = Mathf.Clamp(playerPOS.y, -15f, 15f);
+        //player.position = playerPOS;
+        //transform.LookAt(new Vector3(player.position.x, Mathf.Clamp(player.position.y, -15f, 15f), player.position.z));
 
 
         //rotationY += Input.GetAxis("Mouse Y") * turnSpeed;
