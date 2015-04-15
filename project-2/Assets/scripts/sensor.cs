009d2a8eac6033b9ba9ae2a9eec7de0c4853f44d
Coleten McGuire <s505828@mail.nwmissouri.edu>
2015-04-15 18:20:16 -0500

    level 5

--- project-2/Assets/scripts/sensor.cs
+++ project-2/Assets/scripts/sensor.cs
@@ -5,18 +5,15 @@ public class sensor : MonoBehaviour {
     public Transform toggleObject1;
     public Transform toggleObject2;
     public Transform toggleObject3;
-    public Transform toggleObject4;
     VerticalBlockController firstMovingBlock;
     VerticalBlockController secondMovingBlock;
     VerticalBlockController thirdMovingBlock;
-    VerticalBlockController fourthMovingBlock;
 
 	// Use this for initialization
 	void Start () {
         firstMovingBlock = toggleObject1.GetComponent<VerticalBlockController>();
         secondMovingBlock = toggleObject2.GetComponent<VerticalBlockController>();
         thirdMovingBlock = toggleObject3.GetComponent<VerticalBlockController>();
-        fourthMovingBlock = toggleObject4.GetComponent<VerticalBlockController>();
 	}
 
     void OnTriggerEnter(Collider col)
@@ -34,11 +31,6 @@ public class sensor : MonoBehaviour {
         if (col.gameObject.name == "Top3")
         {
             thirdMovingBlock.isLocked = false;
-        }
-
-        if (col.gameObject.name == "Top4")
-        {
-            fourthMovingBlock.isLocked = false;
         }
 
     }
