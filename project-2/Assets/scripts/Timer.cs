af699e644261df204291208c71ce69b9d026d863
Coleten McGuire <s505828@mail.nwmissouri.edu>
2015-04-24 15:27:00 -0500

    my workspace

--- project-2/Assets/scripts/Timer.cs
+++ project-2/Assets/scripts/Timer.cs
@@ -3,20 +3,21 @@ using System.Collections;
 
 public class Timer : MonoBehaviour {
     public static float timer;
+    private Rect currentRect = new Rect(15, 15, 150, 150);
+    public GUIStyle style;
 	// Use this for initialization
 	void Start () {
         
 	}
     void OnGUI()
     {
+        currentRect.x = (Screen.width * .5f) - (currentRect.width * 0.5f);
         int minutes = Mathf.FloorToInt(timer / 60F);
         int seconds = Mathf.FloorToInt(timer - minutes * 60);
         int milliseconds = (int)((timer - Mathf.Floor(timer)) * 1000);
 
         string Time = string.Format("{0:0}:{1:00}:{2:000}", minutes, seconds, milliseconds);
-        GUI.skin.label.fontSize = 50;
-        GUI.color = Color.black;
-        GUI.Label(new Rect(450, 0, 250, 250), Time);
+        GUI.Label(new Rect(currentRect.x, 0, 250, 250), Time, style);
     }
 	
 	// Update is called once per frame
