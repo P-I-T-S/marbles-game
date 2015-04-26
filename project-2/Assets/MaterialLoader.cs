25b0fc7f4b3744053fb47f731d8e017bee6ad212
Keswiik <littleman74@live.com>
2015-04-26 14:31:07 -0500

    Fixing Issues?

---
+++ project-2/Assets/MaterialLoader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject gameManager = GameObject.FindGameObjectWithTag("Game Manager");
+        if (gameManager != null)
+        {
+            if (gameManager.GetComponent<gameManager>().selectedMaterial >= 0)
+            {
+                GetComponent<MeshRenderer>().material =
+                    gameManager.GetComponent<gameManager>()
+                    .ballMaterials[gameManager.GetComponent<gameManager>().selectedMaterial];
+            }
+        }
+	}
+}
