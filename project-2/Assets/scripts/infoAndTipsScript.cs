3faf4dfe156464a5b3ffa17b8334eb2aea50a8c1
Keswiik <littleman74@live.com>
2015-04-26 22:00:03 -0500

    Level 6

--- project-2/Assets/scripts/infoAndTipsScript.cs
+++ project-2/Assets/scripts/infoAndTipsScript.cs
@@ -7,7 +7,7 @@ public class infoAndTipsScript : MonoBehaviour {
     Text infoAndTipsText;
     int levelName;
     GameObject timer;
-    string[] listOfTips = { "level 0, do not use", "Use the w, a, s and d keys to move to the green platform", "Collect the gems, then go the the finish platform"};
+    string[] listOfTips = { "level 0, do not use", "Use the w, a, s and d keys to move to the green platform", "Collect the gems, then go the the finish platform", "", "", "", "", "", "", "", "Angle floating blocks to reach new parts of the level!"};
 
 	// Use this for initialization
 	void Start () {
