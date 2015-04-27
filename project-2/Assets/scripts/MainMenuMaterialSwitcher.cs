97d77bd4c6f0cb3031b205c3c126566e915d5a07
Keswiik <littleman74@live.com>
2015-04-26 21:30:56 -0500

    Work pls

---
+++ project-2/Assets/scripts/MainMenuMaterialSwitcher.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuMaterialSwitcher : MonoBehaviour {
+
+    public GameObject marble;
+    public GameObject gameManager;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SwitchMaterial(int i)
+    {
+        marble.GetComponent<MeshRenderer>().material = gameManager.GetComponent<gameManager>().ballMaterials[i];
+        gameManager.GetComponent<gameManager>().selectedMaterial = i;
+    }
+}
